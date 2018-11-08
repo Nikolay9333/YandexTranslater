a77ad43c12296644a7d43bf49fc86920e9742c09
niko <zadorozhnyyn@list.ru>
2018-11-08 13:17:48 +0300

    INIT COMMIT

---
+++ YandexTranslater/Controllers/YandexTranslaterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace YandexTranslater.Controllers
+{
+    public class YandexTranslaterController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public YandexTranslaterController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public IActionResult Index()
+        {
+            var token = _configuration.GetValue<string>("translateToken");
+
+            return Json(new {token = token});
+        }
+    }
+}
